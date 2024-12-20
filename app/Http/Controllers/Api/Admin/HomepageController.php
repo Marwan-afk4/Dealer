f4d630a5182a0085f8121a5e02b353131b8081af
Marwan-afk4 <fk413691@gmail.com>
2024-12-20 23:49:51 +0200

    sasasaasa

--- app/Http/Controllers/Api/Admin/HomepageController.php
+++ app/Http/Controllers/Api/Admin/HomepageController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Developer;
-use App\Models\Lead;
 use App\Models\Request as ModelsRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -14,10 +13,6 @@ class HomepageController extends Controller
     public function homepage() {
         $developers = Developer::all();
         $requests = ModelsRequest::all();
-        $leadPending =Lead::where('status','pending')->get();
-        $leadClosed=Lead::where('status','closed')->get();
-        $leadInprogress=Lead::where('status','in_progress')->get();
-        $leadLost=Lead::where('status','lost')->get();
         $users = User::where('role','user')->get();
         $brockers=User::where('role','brocker')->get();
         $admins=User::where('role','admin')->get();
@@ -29,10 +24,6 @@ class HomepageController extends Controller
         $brockers_count=$brockers->count();
         $admins_count=$admins->count();
         $trianers_count=$trianers->count();
-        $leadClosed_count=$leadClosed->count();
-        $leadInprogress_count=$leadInprogress->count();
-        $leadLost_count=$leadLost->count();
-        $leadPending_count=$leadPending->count();
 
         $data=[
             'developers_count'=>$developers_count,
@@ -41,10 +32,6 @@ class HomepageController extends Controller
             'brockers_count'=>$brockers_count,
             'admins_count'=>$admins_count,
             'trianers_count'=>$trianers_count,
-            'leadClosed_count'=>$leadClosed_count,
-            'leadInprogress_count'=>$leadInprogress_count,
-            'leadLost_count'=>$leadLost_count,
-            'leadPending_count'=>$leadPending_count
         ];
 
         return response()->json($data);
