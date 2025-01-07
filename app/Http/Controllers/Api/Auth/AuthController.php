3d61b1eb701bc439cb1e9d70761ab417cb53478a
Marwan-afk4 <fk413691@gmail.com>
2025-01-07 02:14:28 +0200

    governce in signup

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -20,6 +20,7 @@ class AuthController extends Controller
             'password' => 'required|min:6',
             'qualification' => 'nullable',
             'experience_year' => 'nullable|integer',
+            'governce' => 'nullable'
         ]);
         if($validation->fails()) {
             return response()->json(['error' => $validation->errors()], 401);
@@ -31,8 +32,9 @@ class AuthController extends Controller
             'email' => $request->email,
             'phone' => $request->phone,
             'password' => Hash::make($request->password),
-            'qualification' => $request->qualification,
-            'experience_year' => $request->experience_year,
+            'qualification' => $request->qualification??null,
+            'experience_year' => $request->experience_year??null,
+            'governce' => $request->governce??null,
             'role' => 'user',
             'age' => $request->age
         ]);
