a97ccb5f2cf883c019288ac572a858acbef4215d
Marwan-afk4 <fk413691@gmail.com>
2025-01-29 18:13:54 +0200

    a

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\CorsMiddleware;
 use App\Http\Middleware\SuperAdminMiddleware;
 use App\Http\Middleware\UserMiddleware;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -20,6 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'IsSuperAdmin' => SuperAdminMiddleware::class,
             'IsAdmin'=> AdminMiddleware::class,
             'IsUser'=> UserMiddleware::class,
+            'web' => EncryptCookies::class,
+            'start-session' => \Illuminate\Session\Middleware\StartSession::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
